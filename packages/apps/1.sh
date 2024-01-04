3d0f2ce93ab4f7a9d2b1229b5440e29cadc4574e
Andrei Kvapil <kvapss@gmail.com>
2024-01-04 13:04:13 +0100

    aa

---
+++ packages/apps/1.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -e
+file=$1
+charts=$(find . -mindepth 2 -maxdepth 2 -name Chart.yaml | awk 'sub("/Chart.yaml", "")')
+
+# <chart> <version> <commit> 
+new_map=$(
+  for chart in $charts; do
+    awk '/^name:/ {chart=$2} /^version:/ {version=$2} END{printf "%s %s %s\n", chart, version, "HEAD"}' $chart/Chart.yaml
+  done
+)
+
+if [ ! -f "$file" ] || [ ! -s "$file" ]; then
+  echo "$new_map" > "$file"
+  exit 0
+fi
+
+miss_map=$(echo "$new_map" | awk 'NR==FNR { new_map[$1 " " $2] = $3; next } { if (!($1 " " $2 in new_map)) print $1, $2, $3}' - $file)
+
+resolved_miss_map=$(
+  echo "$miss_map" | while read chart version commit; do
+    if [ "$commit" = HEAD ]; then
+      line=$(awk '/^version:/ {print NR; exit}' "$chart/Chart.yaml")
+      change_commit=$(git --no-pager blame -L20,+1 "$chart/Chart.yaml" | awk '{print $1}')
+      commit=$(git describe --always "$change_commit~1")
+    fi
+    echo "$chart $version $commit"
+  done
+)
+
+printf "%s\n" "$new_map" "$resolved_miss_map" | sort -k1,1 -k2,2 -V > "$file"
