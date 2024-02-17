df47d2f4a61b65ae2c7031b4969f4b1eaf3f90a3
Andrei Kvapil <kvapss@gmail.com>
2024-02-17 19:11:09 +0100

    Change tag for matchbox image (#15)
    Signed-off-by: Andrei Kvapil <kvapss@gmail.com>

--- hack/prepare_release.sh
+++ hack/prepare_release.sh
@@ -12,7 +12,7 @@ talos_version=$(awk '/^version:/ {print $2}' packages/core/installer/images/talo
 
 set -x
 
-sed -i "s|\(ghcr.io/aenix-io/cozystack/matchbox:\)v[^ ]\+|\1${version}|g" README.md
+sed -i "s|\(ghcr.io/aenix-io/cozystack/matchbox:\)v[^ ]\+|\1${talos_version}|g" README.md
 sed -i "s|\(ghcr.io/aenix-io/cozystack/talos:\)v[^ ]\+|\1${talos_version}|g" README.md
 
 sed -i "/^TAG / s|=.*|= ${version}|" \
