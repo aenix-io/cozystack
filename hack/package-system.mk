451e124c565ed6d9200c0497c153e0bafa76c923
Marian Koreniuk <moriarti@cp.if.ua>
2024-04-17 23:02:27 +0300

    Update hack/package-system.mk
    Co-authored-by: Andrei Kvapil <kvapss@gmail.com>

--- hack/package-system.mk
+++ hack/package-system.mk
@@ -5,14 +5,7 @@ help: ## Show this help.
 	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {sub("\\\\n",sprintf("\n%22c"," "), $$2);printf "\033[36m%-20s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
 
 show: ## Show output of rendered templates
-	case "$(NAME)" in
-    		cilium|kubeovn)
-        		kubectl get hr -n $(NAMESPACE) $(NAME) -o jsonpath='{.spec.values}' | helm template --dry-run=server -n $(NAMESPACE) $(NAME) . -f -
-        		;;
-    		*)
-        		helm template --dry-run=server -n $(NAMESPACE) $(NAME) .
-        		;;
-	esac
+	kubectl get hr -n $(NAMESPACE) $(NAME) -o jsonpath='{.spec.values}' | helm template --dry-run=server -n $(NAMESPACE) $(NAME) . -f -
 
 apply: suspend ## Apply Helm release to a Kubernetes cluster 
 	kubectl get hr -n $(NAMESPACE) $(NAME) -o jsonpath='{.spec.values}' | helm upgrade -i -n $(NAMESPACE) $(NAME) . -f -
