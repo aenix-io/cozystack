d86c1269eb38a9172eb05d67033ec59504af4f12
Marian Koreniuk <moriarti@cp.if.ua>
2024-04-17 23:02:14 +0300

    Update hack/package-system.mk
    Co-authored-by: Andrei Kvapil <kvapss@gmail.com>

--- hack/package-system.mk
+++ hack/package-system.mk
@@ -18,14 +18,7 @@ apply: suspend ## Apply Helm release to a Kubernetes cluster
 	kubectl get hr -n $(NAMESPACE) $(NAME) -o jsonpath='{.spec.values}' | helm upgrade -i -n $(NAMESPACE) $(NAME) . -f -
 
 diff: ## Diff Helm release against objects in a Kubernetes cluster
-	case "$(NAME)" in
-		cilium|kubeovn)
-			kubectl get hr -n $(NAMESPACE) $(NAME) -o jsonpath='{.spec.values}' | helm diff upgrade --allow-unreleased --normalize-manifests -n $(NAMESPACE) $(NAME) . -f -
-			;;
-		*)
-			helm diff upgrade --allow-unreleased --normalize-manifests -n $(NAMESPACE) $(NAME) .
-			;;
-	esac
+	kubectl get hr -n $(NAMESPACE) $(NAME) -o jsonpath='{.spec.values}' | helm diff upgrade --allow-unreleased --normalize-manifests -n $(NAMESPACE) $(NAME) . -f -
 
 suspend: ## Suspend reconciliation for an existing Helm release
 	flux suspend hr -n $(NAMESPACE) $(NAME)
