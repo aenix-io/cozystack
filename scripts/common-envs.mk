6055de8c52424ef2156eaf6f22246c7bbdd6d715
Floppy Disk <kklinch0@gmail.com>
2024-10-15 21:14:06 +0300

    fix version if work in fort

--- scripts/common-envs.mk
+++ scripts/common-envs.mk
@@ -8,3 +8,9 @@ TAG = $(shell git describe --tags --exact-match 2>/dev/null || echo latest)
 define settag
 $(if $(filter $(TAG),latest),latest,$(1))
 endef
+
+ifeq ($(VERSION),)
+    $(shell git remote add upstream https://github.com/aenix-io/cozystack.git || true)
+    $(shell git fetch upstream --tags)
+    VERSION = $(patsubst v%,%,$(shell git describe --tags --abbrev=0))
+endif
