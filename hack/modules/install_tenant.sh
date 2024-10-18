4512e10c110c746524c0f456087bd4d6d5e83c56
Andrei Kvapil <kvapss@gmail.com>
2024-10-18 10:43:05 +0200

    Refactor e2e tests for applications

--- hack/modules/install_tenant.sh
+++ hack/modules/install_tenant.sh
@@ -3,9 +3,9 @@
 function install_tenant (){
     local release_name="$1"
     local namespace="$2"
-    local gitrepo_name="$3"
-    local flux_ns="$4"
-    local values_file="${5:-tenant.yaml}"
+    local values_file="${3:-tenant.yaml}"
+    local repo_name="cozystack-apps"
+    local repo_ns="cozy-public"
 
-    install_helmrelease "$release_name" "$namespace" "../../packages/apps/tenant" "$gitrepo_name" "$flux_ns" "$values_file"
+    install_helmrelease "$release_name" "$namespace" "tenant" "$repo_name" "$repo_ns" "$values_file"
 }
