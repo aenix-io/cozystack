1ec10165b02847384397ac60d971cd7aa12cd2d0
Andrei Kvapil <kvapss@gmail.com>
2024-10-21 17:29:39 +0200

    Update talos v1.8.1 (#448)

--- hack/e2e.sh
+++ hack/e2e.sh
@@ -114,7 +114,7 @@ machine:
     - name: zfs
     - name: spl
   install:
-    image: ghcr.io/aenix-io/cozystack/talos:v1.8.0
+    image: ghcr.io/aenix-io/cozystack/talos:v1.8.1
   files:
   - content: |
       [plugins]
