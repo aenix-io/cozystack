0ae7db654c86f9242a97283d3f48b9dbb2be5fc8
klinch0 <68821526+klinch0@users.noreply.github.com>
2024-10-24 15:44:19 +0300

    add migration for dv (#455)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Updated versioning logic to support migration from version 5 to
    version 6.
    - Introduced a migration script for managing Persistent Volume Claims
    (PVCs) and updating configurations.
    
    - **Bug Fixes**
    - Ensured error handling and waiting mechanisms for Kubernetes resources
    are preserved during migrations.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -3,7 +3,7 @@ set -o pipefail
 set -e
 
 BUNDLE=$(set -x; kubectl get configmap -n cozy-system cozystack -o 'go-template={{index .data "bundle-name"}}')
-VERSION=5
+VERSION=6
 
 run_migrations() {
   if ! kubectl get configmap -n cozy-system cozystack-version; then
