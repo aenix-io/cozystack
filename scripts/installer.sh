3d2d3659fa5a93f457b81517dd70abd821751089
Floppy Disk <kklinch0@gmail.com>
2024-11-15 17:56:11 +0300

    add keycloak

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -3,7 +3,7 @@ set -o pipefail
 set -e
 
 BUNDLE=$(set -x; kubectl get configmap -n cozy-system cozystack -o 'go-template={{index .data "bundle-name"}}')
-VERSION=7
+VERSION=8
 
 run_migrations() {
   if ! kubectl get configmap -n cozy-system cozystack-version; then
