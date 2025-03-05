920e32d1556f588b935c9954ad4d52e2524712e5
Denis Seleznev <kto.3decb@gmail.com>
2025-03-05 02:48:17 +0100

    Add orphan loop detacher.

--- packages/system/linstor/hack/plunger/plunger-satellite.sh
+++ packages/system/linstor/hack/plunger/plunger-satellite.sh
@@ -15,5 +15,10 @@ while true; do
   # timeout at the start of the loop to give a chance for the fresh linstor-satellite instance to cleanup itself
   sleep 1m
 
-  echo "noop"
+  # Detect orphaned loop devices and detach them
+  # the `/` path could not be a backing file for a loop device, so it's a good indicator of a stuck loop device
+  # TODO describe the issue in more detail
+  losetup --json \
+  | jq -r '.[][] | select(."back-file" == "/ (deleted)") | "losetup --detach \(.name)"' \
+  | sh -x
 done
